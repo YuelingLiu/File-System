43472baeb548e9079752d52eb0ac8d163c42c1f3
Student <kpcrocks@gmail.com>
2022-11-05 16:27:13 -0700

    push and pull

--- mfs.h
+++ mfs.h
@@ -53,7 +53,7 @@ typedef struct
 	unsigned short  d_reclen;		/*length of this record */
 	unsigned short	dirEntryPosition;	/*which directory entry position, like file pos */
 	uint64_t	directoryStartLocation;		/*Starting LBA of directory */
-	uintptr_t  dirp; // I added this directory pointer
+	//uintptr_t  dirp; // I added this directory pointer
 	} fdDir;
 
 struct fdPathResult{
